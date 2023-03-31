30c4e3c13aca094c3333468a5052e1ed032ef5ae
Olar-Alex-Student <117835182+Olar-Alex-Student@users.noreply.github.com>
2023-03-31 12:49:18 +0300

    Change little things on friends grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { StrictMode, useState } from 'react'
 import './App.css'
-import { Text } from '@mantine/core'
 
 function App() {
 
   return (
     <StrictMode>
-      <Text>Merge</Text>
+      
     </StrictMode>
   )
 }
---
+++ src/features/friends/components/friend-grid.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { FRIENDS_API } from "../constants/friend.cont";
+
+export const friendGrid = () => {
+    const [friends, setFirends] = useState("");
+
+    useEffect(() => {
+
+    }, [])
+}
